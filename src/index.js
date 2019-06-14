beaf84cdbb2cb413cdfacd19f5483bf7c90dad70
Josimar <rocha_josimar@yahoo.com.br>
2019-06-13 23:46:31 -0300

    add Hot-reload

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import createStore from './redux-flow/configure-store'
+
+const store = createStore()
+
+const render = () => {
+  return ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'));
+}
+render()
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
