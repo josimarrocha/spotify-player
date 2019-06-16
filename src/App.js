cb6a18f45412160018eb6a0ff32079298526d1e6
Josimar <rocha_josimar@yahoo.com.br>
2019-06-16 12:14:32 -0300

    Add globalStyles and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Routes from './routes'
+import 'materialize-css/dist/css/materialize.css'
+import { GlobaStyle } from './stylesGlobal'
 
 function App() {
   return (
     <div className="App">
+      <GlobaStyle />
+      <Routes />
     </div>
-  );
+  )
 }
 
 export default App;
