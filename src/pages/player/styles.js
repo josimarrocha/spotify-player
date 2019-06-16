1a8b2da7cfcfe0ab7fb2e411ba9008a34f72af31
Josimar <rocha_josimar@yahoo.com.br>
2019-06-16 12:17:48 -0300

    Create styles page Player

---
+++ src/pages/player/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const PlayerMusic = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  .card-content{
+    display:flex
+  }
+  .Player-left{
+    margin-right:10px;
+  }
+  .Player-left > img{
+    max-width: 150px;
+  }
+`
