2cb3613cad553906052fe382e99260f010653d97
Josimar <rocha_josimar@yahoo.com.br>
2019-06-16 12:16:58 -0300

    Create Styles page Home

---
+++ src/pages/home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items:center;
+`
+
+export const SearchBox = styled.div`
+  display: flex;
+`
